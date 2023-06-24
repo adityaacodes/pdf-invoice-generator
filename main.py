88873b9b7778e437bace9a8a6f818c9e0d983f93
Aditya <apocalypse20101@gmail.com>
2023-06-24 21:48:13 +0530

    Corrected date and cleaned the code

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import pandas
 import glob
 from fpdf import FPDF
 from pathlib import Path
-import time
-
-date = time.strftime("%Y.%m.%d")
-print(date)
 
 filepaths = glob.glob("C:/Apps/App4-pdf-invoices/invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pandas.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    filename = Path(filepath).stem
-    invoice_nr = filename.split('-')
-    pdf.set_font(family="Times", style="B", size=10)
-    pdf.cell(w=0, h=10, txt=f"Invoice number {invoice_nr[0]}", align="L", ln=1)
-    pdf.cell(w=0, h=10, txt=f"Date {date}", align="L", ln=1)
-    pdf.output(f"PDFs/{filename}.pdf")
 
+    filename = Path(filepath).stem
+    invoice_nr, date = filename.split('-')
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.cell(w=0, h=10, txt=f"Invoice number {invoice_nr[0]}", ln=1)
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.cell(w=0, h=10, txt=f"Date {date}", ln=1)
+
+    pdf.output(f"PDFs/{filename}.pdf")
