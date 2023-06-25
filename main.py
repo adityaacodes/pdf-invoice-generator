7aa9006abdd73b6e412bb9e5f64c0041a354022e
Aditya <apocalypse20101@gmail.com>
2023-06-25 11:42:43 +0530

    Final Commit

--- main.py
+++ main.py
@@ -23,31 +23,31 @@ for filepath in filepaths:
     columns = [item.replace("_", " ").title() for item in columns]
 
     # Add a header
-    pdf.cell(w=30, h=8, txt=columns[0], align="L", border=1)
-    pdf.cell(w=70, h=8, txt=columns[1], align="L", border=1)
-    pdf.cell(w=31, h=8, txt=columns[2], align="L", border=1)
-    pdf.cell(w=30, h=8, txt=columns[3], align="L", border=1)
-    pdf.cell(w=30, h=8, txt=columns[4], align="L", border=1, ln=1)
+    pdf.cell(w=30, h=8, txt=columns[0], align="C", border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], align="C", border=1)
+    pdf.cell(w=31, h=8, txt=columns[2], align="C", border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], align="C", border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], align="C", border=1, ln=1)
 
     # Adding rows to the table
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=11)
         pdf.set_text_color(30, 30, 30)
-        pdf.cell(w=30, h=8, txt=str(row['product_id']), align="L", border=1)
-        pdf.cell(w=70, h=8, txt=str(row['product_name']), align="L", border=1)
-        pdf.cell(w=31, h=8, txt=str(row['amount_purchased']), align="L", border=1)
-        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), align="L", border=1)
-        pdf.cell(w=30, h=8, txt=str(row['total_price']), align="L", ln=1, border=1)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), align="C", border=1)
+        pdf.cell(w=70, h=8, txt=str(row['product_name']), align="C", border=1)
+        pdf.cell(w=31, h=8, txt=str(row['amount_purchased']), align="C", border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), align="C", border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), align="C", ln=1, border=1)
 
     # Add total price cell
     total_price = df['total_price'].sum()
     pdf.set_font(family="Times", size=10)
     pdf.set_text_color(80, 80, 80)
-    pdf.cell(w=30, h=8, txt="", align="L", border=1)
-    pdf.cell(w=70, h=8, txt="", align="L", border=1)
-    pdf.cell(w=31, h=8, txt="", align="L", border=1)
-    pdf.cell(w=30, h=8, txt="", align="L", border=1)
-    pdf.cell(w=30, h=8, txt=str(total_price), align="R", ln=1, border=1)
+    pdf.cell(w=30, h=8, txt="", align="C", border=1)
+    pdf.cell(w=70, h=8, txt="", align="C", border=1)
+    pdf.cell(w=31, h=8, txt="", align="C", border=1)
+    pdf.cell(w=30, h=8, txt="", align="C", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_price), align="C", ln=1, border=1)
 
     # Add total price sentence
     pdf.set_font(family="Times", style="B", size=15)
